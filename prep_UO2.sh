2d82b332db1d431b24d38dc43d9cad7bf08941b8
Bruce Ravel <bravel@bnl.gov>
2014-12-23 15:58:29 -0500

    first four materials prepped

---
+++ prep_UO2.sh
@@ -0,0 +1,7 @@
+./models.py -f UO2 -6		# feff6
+./models.py -f UO2		# feff8 no self-consistency
+./models.py -f UO2 -s -r 3	# feff8 SCF 1 shell
+./models.py -f UO2 -s -r 4	# feff8 SCF 2 shell
+./models.py -f UO2 -s -r 5	# feff8 SCF 3 shell
+./models.py -f UO2 -s -r 5.5	# feff8 SCF 4 shell
+./models.py -f UO2 -s -r 6	# feff8 SCF 5 shell
---
+++ prep_bromoadamantane.sh
@@ -0,0 +1,3 @@
+./models.py -f bromoadamantane -6	# feff6
+./models.py -f bromoadamantane		# feff8 no self-consistency
+./models.py -f bromoadamantane -s -r 8	# feff8 SCF the whole molecule
