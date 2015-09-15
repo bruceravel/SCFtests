bb4532cda5a9b565036fa98cc599a41069b8ff5f
Bruce Ravel <bravel@bnl.gov>
2015-09-15 11:46:32 -0400

    just about done with new layout

--- o2h.sh
+++ o2h.sh
@@ -7,11 +7,14 @@ target=../SCFtests-ghpages
 all='README use scf iorder'
 for f in $all; do
     org2html -y $f.org
+    ## use sed to insert links.template near the end of each html file
     sed '/class="author"/ r links.template' $f.html > __temp
     mv __temp $f.html
 done
 
+## copy all the files over
 for f in $all; do
     cp -v $f.html $target/
 done
+## rename README.html to index.html
 mv $target/README.html $target/index.html
